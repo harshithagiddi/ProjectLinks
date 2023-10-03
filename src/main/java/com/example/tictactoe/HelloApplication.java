4dc402e2ed47a8fa729f94fef98daa7049d23323
kmit <kmit@gmail.com>
2023-10-03 16:51:53 +0530

    Added Tic tac toe

---
+++ src/main/java/com/example/tictactoe/HelloApplication.java
@@ -0,0 +1,126 @@
+package com.example.tictactoe;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+    private char currentPlayer = 'X';
+    private Button[][] buttons = new Button[3][3];
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane gridPane = new GridPane();
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col] = createButton();
+                gridPane.add(buttons[row][col], col, row);
+            }
+        }
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                int finalRow = row;
+                int finalCol = col;
+                buttons[row][col].setOnAction(e -> handleButtonClick(finalRow, finalCol));
+            }
+        }
+
+        Scene scene = new Scene(gridPane, 300, 300);
+        primaryStage.setTitle("Tic Tac Toe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private Button createButton() {
+        Button button = new Button();
+        button.setMinSize(100, 100);
+        return button;
+    }
+
+    private void handleButtonClick(int row, int col) {
+        if (buttons[row][col].getText().equals("") && !isGameFinished()) {
+            buttons[row][col].setText(String.valueOf(currentPlayer));
+            buttons[row][col].setDisable(true);
+
+            if (checkWin()) {
+                showAlert("Player " + currentPlayer + " wins!");
+                resetGame();
+            } else if (isBoardFull()) {
+                showAlert("It's a draw!");
+                resetGame();
+            } else {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        }
+    }
+
+    private boolean isGameFinished() {
+        return checkWin() || isBoardFull();
+    }
+
+    private boolean checkWin() {
+        for (int i = 0; i < 3; i++) {
+            if (buttons[i][0].getText().equals(String.valueOf(currentPlayer))
+                    && buttons[i][1].getText().equals(String.valueOf(currentPlayer))
+                    && buttons[i][2].getText().equals(String.valueOf(currentPlayer))) {
+                return true;
+            }
+            if (buttons[0][i].getText().equals(String.valueOf(currentPlayer))
+                    && buttons[1][i].getText().equals(String.valueOf(currentPlayer))
+                    && buttons[2][i].getText().equals(String.valueOf(currentPlayer))) {
+                return true;
+            }
+        }
+
+        if (buttons[0][0].getText().equals(String.valueOf(currentPlayer))
+                && buttons[1][1].getText().equals(String.valueOf(currentPlayer))
+                && buttons[2][2].getText().equals(String.valueOf(currentPlayer))) {
+            return true;
+        }
+        if (buttons[0][2].getText().equals(String.valueOf(currentPlayer))
+                && buttons[1][1].getText().equals(String.valueOf(currentPlayer))
+                && buttons[2][0].getText().equals(String.valueOf(currentPlayer))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (buttons[row][col].getText().equals("")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Game Over");
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    private void resetGame() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                buttons[row][col].setText("");
+                buttons[row][col].setDisable(false);
+            }
+        }
+        currentPlayer = 'X';
+    }
+}
